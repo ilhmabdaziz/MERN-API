d172feb0950303b17383d3bbff61a516e198c930
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-09-20 23:07:58 +0700

    add routes restructure

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express"); // nodeJs
 // import express from "express"; // ES6
 
 const app = express();
+const productsRoutes = require("./src/routes/products");
 
-app.use(() => {
-  console.log("hello server...");
-});
+app.use("/", productsRoutes);
 
 app.listen(4000);
