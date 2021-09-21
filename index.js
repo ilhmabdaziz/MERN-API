8c6e60d0519df4b24d8fd3cd4ce42b50ad6282af
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-09-21 12:08:16 +0700

    add cors

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const express = require("express"); // nodeJs
 const app = express();
 const productsRoutes = require("./src/routes/products");
 
-app.use("/", productsRoutes);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Header", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/v1/customer", productsRoutes);
 
 app.listen(4000);
