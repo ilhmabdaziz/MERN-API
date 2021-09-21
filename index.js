2f8af89d21642e3e51028d29532f353731b6fac1
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-09-21 18:24:32 +0700

    Validatorreq body and error response, end point and api-planning

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express"); // nodeJs
 // import express from "express"; // ES6
+const bodyParser = require("body-parser");
 
 const app = express();
-const productsRoutes = require("./src/routes/products");
+const authRoutes = require("./src/routes/auth");
+const blogRoutes = require("./src/routes/blog");
 
+// body-parser
+app.use(bodyParser.json()); // type JSON
+
+// CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -14,6 +20,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/v1/customer", productsRoutes);
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({
+    message: message,
+    data: data,
+  });
+});
 
 app.listen(4000);
