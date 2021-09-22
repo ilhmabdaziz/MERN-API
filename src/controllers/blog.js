f8b8d302663b11d492287162df8a8d25f0f0f1b5
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-09-22 15:14:37 +0700

    added paggination for Response API

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -46,11 +46,25 @@ exports.createBlogPost = (req, res, next) => {
 };
 
 exports.getAllBlogPost = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = req.query.perPage || 5;
+  let totalItems;
+
   BlogPost.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return BlogPost.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((result) => {
       res.status(200).json({
         message: "Data Blog Post Berhasil dipanggil",
         data: result,
+        total_data: totalItems,
+        per_page: perPage,
+        current_page: currentPage,
       });
     })
     .catch((err) => {
